f01c6893493f4f3e9bfcd14f98010bf8bb156a17
curtis <curtis@flatterline.com>
2011-03-07 12:50:02 -0700

    Version bump to 0.1.5

--- lib/is_reviewable/review.rb
+++ lib/is_reviewable/review.rb
@@ -24,27 +24,27 @@ module IsReviewable
     alias :owner  :reviewer
     
     # Named scopes: Order.
-    scope :in_order,            :order => 'created_at ASC'
-    scope :most_recent,         :order => 'created_at DESC'
-    scope :lowest_rating,       :order => 'rating ASC'
-    scope :highest_rating,      :order => 'rating DESC'
+    scope :in_order,            :order => 'reviews.created_at ASC'
+    scope :most_recent,         :order => 'reviews.created_at DESC'
+    scope :lowest_rating,       :order => 'reviews.rating ASC'
+    scope :highest_rating,      :order => 'reviews.rating DESC'
     
     # Named scopes: Filters.
-    scope :since,               lambda { |created_at_datetime|  {:conditions => ['created_at >= ?', created_at_datetime]} }
+    scope :since,               lambda { |created_at_datetime|  { :conditions => ['reviews.created_at >= ?', created_at_datetime]} }
     scope :recent,              lambda { |arg|
                                         if [::ActiveSupport::TimeWithZone, ::DateTime].any? { |c| arg.is_a?(c) }
-                                          {:conditions => ['created_at >= ?', arg]}
+                                          {:conditions => ['reviews.created_at >= ?', arg]}
                                         else
                                           {:limit => arg.to_i}
                                         end
                                       }
     scope :between_dates,       lambda { |from_date, to_date|     {:conditions => {:created_at => (from_date..to_date)}} }
     scope :with_rating,         lambda { |rating_value_or_range|  {:conditions => {:rating => rating_value_or_range}} }
-    scope :with_a_rating,       :conditions => ['rating IS NOT NULL']
-    scope :without_a_rating,    :conditions => ['rating IS NULL']
-    scope :with_a_body,         :conditions => ['body IS NOT NULL AND LENGTH(body) > 0']
-    scope :without_a_body,      :conditions => ['body IS NULL OR LENGTH(body) = 0']
-    scope :complete,            :conditions => ['rating IS NOT NULL AND body IS NOT NULL AND LENGTH(body) > 0']
+    scope :with_a_rating,       :conditions => ['reviews.rating IS NOT NULL']
+    scope :without_a_rating,    :conditions => ['reviews.rating IS NULL']
+    scope :with_a_body,         :conditions => ['reviews.body IS NOT NULL AND LENGTH(reviews.body) > 0']
+    scope :without_a_body,      :conditions => ['reviews.body IS NULL OR LENGTH(reviews.body) = 0']
+    scope :complete,            :conditions => ['reviews.rating IS NOT NULL AND reviews.body IS NOT NULL AND LENGTH(reviews.body) > 0']
     scope :of_reviewable_type,  lambda { |type|       {:conditions => Support.polymorphic_conditions_for(type, :reviewable, :type)} }
     scope :by_reviewer_type,    lambda { |type|       {:conditions => Support.polymorphic_conditions_for(type, :reviewer, :type)} }
     scope :on,                  lambda { |reviewable| {:conditions => Support.polymorphic_conditions_for(reviewable, :reviewable)} }
